09c1f5ef81d65633451e0b2b8df3361d3469ce10
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-03-15 11:24:13 +0530

    Authentication through github added

--- beatly-v1/app/page.tsx
+++ beatly-v1/app/page.tsx
@@ -1,9 +1,14 @@
+
 import Image from "next/image";
+import Appbar from "./Components/Appbar";
+import { SessionProvider } from "next-auth/react";
+import {getServerSession} from "next-auth";
 
 export default function Home() {
   return (
-    <main className="">
-       Beatly
-    </main>
+    
+      <Appbar/>
+    
+    
   );
 }
