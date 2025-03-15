5aa8b294d2d93b88189ac874acd5aff59643909d
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-03-16 00:09:21 +0530

    Sign In and Sign Up passed the testing stage

--- beatly-v1/app/api/auth/[...nextauth]/route.ts
+++ beatly-v1/app/api/auth/[...nextauth]/route.ts
@@ -1,6 +1,7 @@
 import NextAuth from "next-auth";
 import { NextAuthOptions } from "next-auth";
 import GitHubProvider from "next-auth/providers/github";
+import prisma from "@/app/lib/db";
 
 
 
@@ -11,7 +12,31 @@ const authHandler = NextAuth({
             clientId: process.env.GITHUB_CLIENT_ID || "",
             clientSecret: process.env.GITHUB_CLIENT_SECRET || ""
         })
-    ]
+    ],
+    callbacks:{
+        async signIn(params){
+
+            if(!params.user.email){
+                return false;
+            }
+            try{
+
+                await prisma.user.create({
+                    data:{
+                        email: await params.user.email,
+                        //@ts-ignore
+                        provider:"Github"
+                    }
+                    
+                })
+            }catch(err: any){
+                console.log(`Error in Sign In: ${err.message}`);
+            }
+            
+
+            return true;
+        }
+    }
 })
 
 
--- beatly-v1/app/lib/db.ts
+++ beatly-v1/app/lib/db.ts
@@ -1,6 +1,6 @@
 
 // Will be creating a single prisma client
-import { PrismaClient } from "@prisma/client/";
+import { PrismaClient } from "@prisma/client";
 
 const prismaClientSingleton = () => {
     return new PrismaClient();
