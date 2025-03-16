96805ff28c9446d556035d1c0cb591dbec018cbd
Aaditya Bir Singh <aadi.bir.singh@gmail.com>
2025-03-16 11:13:16 +0530

    All api routes configured and tested

--- beatly-v1/app/api/streams/route.ts
+++ beatly-v1/app/api/streams/route.ts
@@ -2,9 +2,14 @@ import { NextRequest, NextResponse } from "next/server"
 import {z} from "zod"
 import prisma from "@/app/lib/db";
 import { url } from "inspector";
+//@ts-ignore
+import youtubesearchapi from "youtube-search-api";
+import Stream from "stream";
+//@ts-ignore
+import youtubeURL from "youtube-url";
 
 
-const YT_REGEX = new RegExp("^(?:https?:\/\/)?(?:www\.)?(?:youtube\.com\/watch\?v=|youtu\.be\/)([\w-]+)$");
+
 
 
 
@@ -26,34 +31,57 @@ export async function POST(req: NextRequest){
     try{
         const data = CreateStreamSchema.parse(await req.json());
 
-        const isYoutube = YT_REGEX.test(data.url);
+        const isYoutube = youtubeURL.valid(data.url);
 
         if(!isYoutube){
-            NextResponse.json({
+            return NextResponse.json({
                 message:"URL must be from Youtube or Spotify."
             },{
                 status:401
             })
         }
         // other wise
-        const extractedId = data.url.split("?v=")[1];
+        const extractedId = youtubeURL.extractId(data.url);
+
+        // getting the video details 
+        const ytResponse = await youtubesearchapi.GetVideoDetails(extractedId);
+        console.log(ytResponse.title);
+        console.log(ytResponse.thumbnail.thumbnails);
+        console.log(JSON.stringify(ytResponse.thumbnail.thumbnails));
+
+        // Extracting the thumbnails
+        const thumbnails = ytResponse.thumbnail.thumbnails;
+        thumbnails.sort((a: {width:number},b: {width:number}) => a.width < b.width ? -1 : 1);  // this is sorted in place.
+
         
         // db call
-        await prisma.stream.create({
+        const stream = await prisma.stream.create({
             data:{
                 userId: data.creatorId,
                 url: data.url,
                 extractedId,
-                type: "Youtube"  // Beacuse for now we only support youtube.
+                type: "Youtube",  // Beacuse for now we only support youtube.
+                title: ytResponse.title ?? "No Title Found",
+                smallImg: thumbnails.length > 1 ? thumbnails[thumbnails.length - 2 ].url : thumbnails[thumbnails.length - 1 ].url ?? "",
+                bigImg: thumbnails[thumbnails.length - 1 ].url ?? "",
+
             }
         })
+        return NextResponse.json({
+            message:"Stream created successfully",
+            id: stream.id
+        },{
+            status:201
+        })
     }catch(err){
-        NextResponse.json({
+        return NextResponse.json({
             message:`Error in creating stream ${err}`
         },{
             status:411
         })
     }
+
+    
     
 
 
@@ -75,3 +103,5 @@ export async function GET(req: NextRequest){
         streams
     })
 }
+
+
